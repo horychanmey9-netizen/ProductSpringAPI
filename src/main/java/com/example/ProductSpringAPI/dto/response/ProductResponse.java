af258515bcb0b0ba09c37e1964d743a0e069e0e2
Nor Chanden <norchanden97@gmail.com>
2026-07-10 16:28:24 +0700

    connect database

--- src/main/java/com/example/ProductSpringAPI/dto/response/ProductResponse.java
+++ src/main/java/com/example/ProductSpringAPI/dto/response/ProductResponse.java
@@ -1,4 +1,12 @@
 package com.example.ProductSpringAPI.dto.response;
 
+import lombok.Data;
+
+@Data
 public class ProductResponse {
+    private Long id;
+    private String proName;
+    private Integer qty;
+    private Double price;
+    private String image;
 }
--- src/main/java/com/example/ProductSpringAPI/repository/ProductRepository.java
+++ src/main/java/com/example/ProductSpringAPI/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.ProductSpringAPI.repository;
 
-public interface ProductRepository {
+import com.example.ProductSpringAPI.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
