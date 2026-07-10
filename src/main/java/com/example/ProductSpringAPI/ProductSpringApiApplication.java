8f110f75067ab5ab9fb2e175e1c3e88223bc4760
chan <horychanmey9@gmail.com>
2026-07-10 14:38:56 +0700

    Initial commit

---
+++ src/main/java/com/example/ProductSpringAPI/ProductSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductSpringAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductSpringAPI/ProductSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductSpringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
