181f2f9538a806ea5bb2402c4cc00b2332603747
Nor Chanden <norchanden97@gmail.com>
2026-07-11 14:19:42 +0700

    Global Exception

--- src/main/java/com/example/ProductSpringAPI/exception/GlobalException.java
+++ src/main/java/com/example/ProductSpringAPI/exception/GlobalException.java
@@ -1,5 +1,16 @@
 package com.example.ProductSpringAPI.exception;
 
-public class GlobalException
-{
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(ProductNotFound.class)
+    public ResponseEntity<?> handleProductNotFound(Exception e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
 }
